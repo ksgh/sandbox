3a0dbae307afc60a7052e4b7e72d74004fa1eaac
kshenk1 <k.shenk@gmail.com>
2021-08-17 16:39:20 -0400

    getting things back in order again

--- docker-devcontainer/build.sh
+++ docker-devcontainer/build.sh
@@ -8,6 +8,6 @@ image_name="sandbox-devel:1.0"
 
 docker build --no-cache -t $image_name . \
     --build-arg BASE_IMAGE="alpine" \
-    --build-arg BASE_TAG="3.13.5"
+    --build-arg BASE_TAG="3.14"
 
 exit $?
